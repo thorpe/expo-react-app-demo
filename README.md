e8edd474941c893969b1088914cad2052d2d1c9d
Thorpe <koangbok@gmail.com>
2020-02-23 20:20:33 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# expo-react-app-demo
-expo-react-app-demo
+01. Requirement
+```
+sign-up on expo.io
+```
+
+install
+```
+npm i -g create-react-native-app
+create-react-native-app expo-react-app-demo
+
+cd expo-react-app-demo
+
+git clone
+```
+
+choose 
+```
+npm i -g create-react-native-app
+create-react-native-app expo-react-app-demo
+
+cd expo-react-app-demo
+
+git clone
+```
+
+build
+```
+expo build:ios
+expo build:android
+```
---
+++ src/Containers/HomeScreen.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  TextInput
+} from 'react-native';
+
+interface IProps {
+
+}
+
+interface IState {
+  text: string
+}
+
+export default class HomeScreen extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      text: "dsdfs"
+    }
+  }
+
+
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>{this.state.text}</Text>
+        <Text>ssssssss</Text>
+      </View>
+    )
+  }
+}
